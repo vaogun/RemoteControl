5d838281837219281880be12eec1735ebd603166
vaogun <vaogun@gmail.com>
2015-06-18 12:01:37 -0500

    Add "chapter 24: styles and includes"

---
+++ app/src/main/java/com/vaojr/android/remotecontrol/RemoteControlActivity.java
@@ -0,0 +1,24 @@
+package com.vaojr.android.remotecontrol;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+
+
+public class RemoteControlActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return new RemoteControlFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
+    }
+
+}
